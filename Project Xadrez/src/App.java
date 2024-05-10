92f88cda6b692b021dcacb2365a65264cb8fcfff
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-05-10 12:48:30 -0300

    teste

--- Project Xadrez/src/App.java
+++ Project Xadrez/src/App.java
@@ -1,5 +1,10 @@
+import boardgame.Position;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Position position = new Position(5, 2);
+        System.out.println(position);
+
     }
 }
